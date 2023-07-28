b1254e53c97a48c30c05a4f23ea0e90dacef5b8c
devJorgeGomez <jorgegomez98125@gmail.com>
2023-07-27 22:34:42 -0500

    Cambio de HtmlValidator como la clase Main y eliminación de Main

--- src/HtmlValidator.java
+++ src/HtmlValidator.java
@@ -1,3 +1,5 @@
+import java.io.IOException;
+import java.util.LinkedList;
 import java.util.Queue;
 import java.util.Stack;
 
@@ -28,5 +30,17 @@ public class HtmlValidator {
             }
         }
         return validator;
+    }
+    public static void main(String[] args) throws IOException {
+        Queue<HtmlTag> queuee = new LinkedList<HtmlTag>();
+        String filepath = "src/index.html";
+
+        queuee=HtmlReader.getTagsFromHtmlFile(filepath);
+        System.out.println(queuee);
+
+        System.out.println();
+
+        Stack<HtmlTag> val = isValidHtml(queuee);
+        System.out.println(val);
     }
 }
